404b515ecaa004235d7976c967aef1117340f723
devagya-rattan <rattandevagya.com>
2025-12-17 12:17:23 +0530

    updated file path

--- 2. Node_DHT_Service/index.js
+++ 2. Node_DHT_Service/index.js
@@ -2,9 +2,9 @@ const express = require("express")
 const awsIot = require('aws-iot-device-sdk');
 let response;
 const device = awsIot.device({
-    keyPath: "/home/devagya/Desktop/arduino/4c31c114ae2216c81d535290379e16e71485863a6c37992a5d0e665e168bb68e-private.pem.txt",
-    certPath: "/home/devagya/Desktop/arduino/4c31c114ae2216c81d535290379e16e71485863a6c37992a5d0e665e168bb68e-certificate.pem.txt",
-    caPath: "/home/devagya/Desktop/arduino/AmazonRootCA1.txt",
+    keyPath: "app/private.pem.txt",
+    certPath: "app/certificate.pem.txt",
+    caPath: "app/AmazonRootCA1.txt",
     clientId: "DHT-Service",
     host: "at5bqbmqwo924-ats.iot.ap-south-1.amazonaws.com",
     reconnectPeriod: 1000,
